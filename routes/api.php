7b5469ad89dbc2661e908590fdf21076c5500865
RogLag <hugo.rog@icloud.com>
2024-11-23 16:11:15 +0100

    🚧 Worked on AvatarController for user avatar management and update API routes

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AvatarController;
+
+// Obtain the avatar of a user
+Route::get('/user/avatar/{user}', [AvatarController::class, 'show'])
+->middleware('throttle:120,1')
+->name('user.avatar.show');
+
+// Update the avatar of a user
+Route::post('/user/avatar', [AvatarController::class, 'store'])
+->middleware(['auth', 'verified'])->name('user.avatar.update');
