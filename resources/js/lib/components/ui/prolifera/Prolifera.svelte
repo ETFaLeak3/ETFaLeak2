37c2b1b18e5b15b3bfa002b4c4db92fece7653ca
Keima <158515677+KylianLeplant@users.noreply.github.com>
2024-12-06 03:27:40 +0100

    animations + sprites

--- resources/js/lib/components/ui/prolifera/Prolifera.svelte
+++ resources/js/lib/components/ui/prolifera/Prolifera.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
     let { className, onClick } : { className?: string, onClick: any } = $props();
+    let frame = $state(1);
+    setInterval(() => {
+        frame = (frame) % 4 + 1;
+    }, 1000);
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <div style="position: relative;" class={`relative select-none hover:cursor-pointer ${className}`}>
-    <img src="/sprites/Spriterouge.png" alt="Spriterouge" class={`w-fit h-auto ${className}`}>
+    <img src="/sprites/Spriterouge{frame}.png" alt="Spriterouge" class={`w-fit h-auto ${className}`}>
     <!-- svelte-ignore a11y_no_static_element_interactions -->
     <div 
         style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" 
